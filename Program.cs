95c06386e0d8ced2ddc020ab2eb3b5d565fbc587
msla-InspireSA <80754340+msla-InspireSA@users.noreply.github.com>
2022-02-01 21:49:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace transilerationapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
